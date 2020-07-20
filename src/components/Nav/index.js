182bd61e22bcd2318bdc76dc82b80aef0a02d5b3
Angel Laguna Garrido <al4guna@gmail.com>
2020-07-19 23:16:00 -0500

    creating Nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {
+	Header, Title
+} from './styled'
+
+export const Nav = () => {
+	return (
+		<Header>
+			<Title>Sistema de gestión de órdenes</Title>
+		</Header>
+	)
+}
---
+++ src/components/Nav/styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+	width: 100%;
+	-webkit-box-shadow: 0px 1px 5px -1px rgba(0,0,0,0.75);
+	-moz-box-shadow: 0px 1px 5px -1px rgba(0,0,0,0.75);
+	box-shadow: 0px 1px 5px -1px rgba(0,0,0,0.75);
+	text-align: center;
+	background: #fff;
+`
+
+export const Title = styled.h1`
+	color: #3578e5;
+	font-size: 0.9em;
+	padding:  20px 0;
+	font-weight: 600;
+`
